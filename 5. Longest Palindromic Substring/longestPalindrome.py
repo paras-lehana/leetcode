fdbea66b0ba540c775925cd5a3ae5f8f12d7ff60
Paras Lehana <paraskumarlehana602@gmail.com>
2024-01-26 19:31:22 +0530

    Update longestPalindrome.py with new method

--- 5. Longest Palindromic Substring/longestPalindrome.py
+++ 5. Longest Palindromic Substring/longestPalindrome.py
@@ -1,4 +1,4 @@
-# rebuild from mind logic, also take care of case2 (cbbd) 
+# for bacabab, decide the right way as j is not 4 but 3 for u, can we have a infinite loop with j handling instead of range tuple
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
@@ -6,13 +6,10 @@ class Solution:
         DEBUG = True
 
         slen = len(s)
-        max_len = 0
         max_p = ""
-        
         
         for i in range(slen):
 
-            plen = 0
             h1 = ""
             h2 = "" 
             offset = 0
@@ -21,36 +18,38 @@ class Solution:
 
             for j in range(slen - 1, i - 1, -1):
 
-                print(f"\n{s=} | {i=} | {j=} | {offset=} | {h1=} | {h2=} | {plen=}\n") if DEBUG else None 
+                print(f"\n{s=} | {i=} | {j=} | {offset=} | {h1=} | {h2=} | Comparing {s[i+offset]} <-> {s[j]}\n") if DEBUG else None 
 
                 if s[i + offset] == s[j]:
 
                     print(f"\nCHAR MATCH! s[{i+offset}] == s[{j}] => {s[i+offset]} = {s[j]} | \n") if DEBUG else None 
-
-                    h2 = s[j] + h2
-
-                    plen = offset + len(h2)
                     
-                    if i + offset >= j:
-                        break
-                    
+
                     h1 = h1 + s[i + offset]
+
+                    if i + offset == j:
+                        break
+
+                    h2 = s[j] + h2  
+                    
                     offset = offset + 1
 
+                    if i + offset == j:
+                        break
+                    
+
                 else:
+                    if offset != 0:
+                        print("yay")
+                        j = j + 1
+                        print(j)
                     offset = 0
                     h1 = h2 = ""
-
-                if i + offset >= j:
-                    break
-
-
-            if plen > max_len:
-                
-                max_len = plen
+ 
+            if len(h1 + h2) > len(max_p):
                 max_p = h1 + h2
 
-                print(f"\nUPDATE! {max_len=} | {max_p=}\n") if DEBUG else None
+                print(f"\nUPDATE! {max_p=}\n") if DEBUG else None
 
         return max_p
             
